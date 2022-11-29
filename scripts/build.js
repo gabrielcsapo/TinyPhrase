879be51559fc49630c8f0561e257c748e47a9b55
Gabriel Csapo <gabecsapo@gmail.com>
2022-11-28 20:01:47 -0800

    feat: adds 3101 options with 5 categories

--- scripts/build.js
+++ scripts/build.js
@@ -7,16 +7,29 @@ const promptFiles = fs.readdirSync(promptDir);
 
 let menuOptionsTxt = ["const MenuOption MenuOptions[] {"];
 
+let largestOptionsParsed = 0;
+let largestOptionsParsedPromptName = '';
+let largestOptionLength = 0;
 let optionsParsed = 0;
 
 promptFiles.forEach((promptFile, index) => {
   const content = fs.readFileSync(path.resolve(promptDir, promptFile), "utf8");
   const listOfOptions = content
-    .split("\n");
+    .split("\n")
+    .map((option) => {
+      if(largestOptionLength < option.length) {
+        largestOptionLength = option.length;
+      }
+      return option.replace(new RegExp('"', "g"), "'").replace(new RegExp("'", "g"), "\'")
+    });
+  if(largestOptionsParsed < listOfOptions.length) {
+    largestOptionsParsed = listOfOptions.length;
+    largestOptionsParsedPromptName = promptFile.replace('.txt', '');
+  }
   optionsParsed += listOfOptions.length;
   menuOptionsTxt.push(`\t{${index}, "${promptFile.replace(".txt", "")}", {${listOfOptions
     .map((option) => `"${option}"`)
-    .join(",")}}}`);
+    .join(",")}}},`);
 });
 
 menuOptionsTxt.push("};");
@@ -25,4 +38,6 @@ console.log(menuOptionsTxt.join('\n'));
 
 console.log('-----------------------------');
 console.log('options parsed: ', optionsParsed);
+console.log('menu with them most options', largestOptionsParsed, ' :: ', largestOptionsParsedPromptName)
+console.log('largest option name is', largestOptionLength);
 console.log('byte size: ', Buffer.byteLength(menuOptionsTxt.join('\n'), "utf-8"));
